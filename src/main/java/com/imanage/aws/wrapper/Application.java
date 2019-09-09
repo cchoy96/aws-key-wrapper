e10fc7d6027432c445c9319ed22e87552c3b4abd
Chris Choy <contact@chrischoy.net>
2019-09-09 16:51:32 -0500

    functions complete. need to setup keys and credentials for usage

--- src/main/java/com/imanage/aws/wrapper/Application.java
+++ src/main/java/com/imanage/aws/wrapper/Application.java
@@ -1,39 +1,80 @@
 package com.imanage.aws.wrapper;
 
-
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.encryptionsdk.AwsCrypto;
-import com.amazonaws.encryptionsdk.CryptoResult;
-import com.amazonaws.encryptionsdk.kms.KmsMasterKey;
+import com.amazonaws.encryptionsdk.MasterKeyProvider;
+import com.amazonaws.encryptionsdk.jce.JceMasterKey;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
+import com.amazonaws.encryptionsdk.multi.MultipleProviderFactory;
+import com.amazonaws.regions.InMemoryRegionImpl;
+import com.amazonaws.regions.Region;
 
-import java.util.Collections;
-import java.util.Map;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+// https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/java-example-code.html
 
 public class Application {
-    private static String keyARN;
-    private static String data;
+    private static PublicKey publicEscrowKey; // todo set these
+    private static PrivateKey privateEscrowKey;
 
     public static void main(final String[] args) {
-        keyARN = args[0];
-        data = args[1];
+        String data = "sampleDatatoEncrypt";
 
+        Application app = new Application();
+        app.decryptBytes(app.encryptBytes(data.getBytes()));
+        app.escrowDecryptBytes(app.encryptBytes(data.getBytes()));
+    }
+
+    private KmsMasterKeyProvider getMasterKeyProvider() {
+        AWSCredentialsProvider creds = new DefaultAWSCredentialsProviderChain(); // todo set AWS_ACCESS_KEY & AWS_SECRET_KEY
+        Region region = new Region(new InMemoryRegionImpl("us-east-1", "domain"));
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        String keyId = "Example";
+        return new KmsMasterKeyProvider(creds, region, clientConfig, keyId);
+    }
+
+    public byte[] encryptBytes(final byte[] plaintext) {
         final AwsCrypto crypto = new AwsCrypto();
-        final KmsMasterKeyProvider prov = new KmsMasterKeyProvider(keyARN);
+        final KmsMasterKeyProvider kms = getMasterKeyProvider();
+
+        // Generate key provider factory using public key
+        final JceMasterKey escrowPub = JceMasterKey.getInstance(publicEscrowKey, null, "Escrow", "Escrow",
+                "RSA/ECB/OAEPWithSHA-512AndMGF1Padding");
+        final MasterKeyProvider<?> provider = MultipleProviderFactory.buildMultiProvider(kms, escrowPub);
 
         // Encrypt the data
-        final Map<String,String> context = Collections.singletonMap("Example", "String");
-        final String ciphertext = crypto.encryptString(prov, data, context).getResult();
-        System.out.println("Ciphertext: " + ciphertext);
+        byte[] ciphertext = crypto.encryptData(provider, plaintext).getResult();
+
+        System.out.println("Ciphertext: " + Arrays.toString(ciphertext));
+        return ciphertext;
+    }
+
+    public byte[] decryptBytes(final byte[] ciphertext) {
+        final AwsCrypto crypto = new AwsCrypto();
+        final KmsMasterKeyProvider kms = getMasterKeyProvider();
+
+        // Generate key provider factory using public key
+        final JceMasterKey escrowPub = JceMasterKey.getInstance(publicEscrowKey, null, "Escrow", "Escrow",
+                "RSA/ECB/OAEPWithSHA-512AndMGF1Padding");
+        final MasterKeyProvider<?> provider = MultipleProviderFactory.buildMultiProvider(kms, escrowPub);
 
         // Decrypt the data
-        final CryptoResult<String, KmsMasterKey> decryptResult = crypto.decryptString(prov, ciphertext);
-        if (!decryptResult.getMasterKeyIds().get(0).equals(keyARN)) {
-            throw new IllegalStateException("Wrong key ID!");
-        }
-        for (final Map.Entry<String,String> e : context.entrySet()) {
-            if (!e.getValue().equals(decryptResult.getEncryptionContext().get(e.getKey()))) {
-                throw new IllegalStateException("Wrong encryption context!");
-            }
-        }
-    })
+        byte[] plaintext = crypto.decryptData(provider, ciphertext).getResult();
+
+        System.out.println("Decrypted: " + Arrays.toString(plaintext));
+        return plaintext;
+    }
+
+    public byte[] escrowDecryptBytes(final byte[] ciphertext) {
+        final AwsCrypto crypto = new AwsCrypto();
+        final JceMasterKey escrowPriv = JceMasterKey.getInstance(publicEscrowKey, privateEscrowKey,"Escrow", "Escrow",
+                "RSA/ECB/OAEPWithSHA-512AndMGF1Padding");
+        byte[] plaintext = crypto.decryptData(escrowPriv, ciphertext).getResult();
+        System.out.println("Decrypted: " + Arrays.toString(plaintext));
+        return plaintext;
+    }
 }
